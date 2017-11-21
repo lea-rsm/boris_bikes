4f39ee8d6bdd23579b03d1d087c2e1615fe4d637
Léa R <lea.rosamond@gmail.com>
2017-11-21 16:36:40 +0000

    dockingstation releases working bikes all test pass

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,4 +2,9 @@ require 'dockingstation'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
+  it 'releases bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
