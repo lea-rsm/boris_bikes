3d9ea55882a90d160c19eb79b9d1044d09929beb
Léa R <lea.rosamond@gmail.com>
2017-11-21 16:06:07 +0000

    bike and dockingstation spec modified

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,7 +1,5 @@
 require 'dockingstation'
 
 describe DockingStation do
-  it 'respond to the method release_bike' do
-    expect(DockingStation).to respond_to(:release_bike)
-  end
+  it { is_expected.to respond_to :release_bike }
 end
