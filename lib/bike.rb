b9ea612d142cea863cbc5987d5fc410f8037789c
Léa R <lea.rosamond@gmail.com>
2017-11-24 13:46:46 +0000

    Reporting a broken bike in bike_spec and bike.rb

--- lib/bike.rb
+++ lib/bike.rb
@@ -5,4 +5,12 @@ class Bike
   def working?
     true
   end
+
+  def report_broken #you call it on a bike, like this when you dock it, they'll register it as a broken bike
+    @broken = true
+  end
+
+  def broken?
+    @broken
+  end
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,6 +1,12 @@
 require "bike"
 
-describe Bike do
+  describe Bike do
   it { is_expected.to respond_to :working? }
 
+  it 'reports broken bike' do
+  bike = Bike.new
+  bike.report_broken
+  expect(bike).to be_broken
+
+  end
 end
